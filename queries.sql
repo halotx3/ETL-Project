3659b5d87affe6edb89925fd351f9ac502f1f784
slcassidy <samantha.l.cassidy@gmail.com>
2020-05-27 19:12:26 -0400

    update info

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE movies_db;
 
 --drop table netflix_data;
---drop table netflix_data;
+--drop table streaming_data;
 
 CREATE TABLE netflix_data (
-id INT PRIMARY KEY,
+show_id INT PRIMARY KEY,
 title VARCHAR,
 duration VARCHAR,
 rating VARCHAR
@@ -12,12 +12,12 @@ rating VARCHAR
 
 
 CREATE TABLE streaming_data (
-show_id INT PRIMARY KEY,
+id INT PRIMARY KEY,
 title VARCHAR,
 movie_year VARCHAR,
 age VARCHAR,
 imdb FLOAT,
-rotten_tomato INT,
+rotten_tomato VARCHAR,
 netflix INT
 );
 	
